48a4e059eebbb47cc92b99fe7e811ffdba06c82e
John Dziurlaj <john@turnout.rocks>
2024-12-10 07:13:21 -0500

    Support an array field containing enum constraint

--- src/constraints/enum.js
+++ src/constraints/enum.js
@@ -1,13 +1,17 @@
+const isArray = require('lodash/isArray')
+
 // Module API
 
 function checkEnum(constraint, value) {
   if (value === null) {
     return true;
   }
-  if (constraint.includes(value)) {
-    return true;
+  if (!isArray(value)) {
+    return (constraint.includes(value))
+  } else {
+    return (value.every(o => constraint.indexOf(o) !== -1))
   }
-  return false;
+
 }
 
 module.exports = {
